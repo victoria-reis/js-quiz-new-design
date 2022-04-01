48505d66abfc2a6f803b3e4fa0970ec030ef6042
theDapperFoxtrot <60763515+theDapperFoxtrot@users.noreply.github.com>
2022-04-01 16:06:47 -0400

    Starting content
    using first object in data set (will expand)

--- script/script.js
+++ script/script.js
@@ -40,3 +40,72 @@ Stretch goals:
     - HMTL
     - PHP
 */
+
+const techTestApp = {};
+
+techTestApp.apiKey = "GyC8jhRUYRRfOwH3Qnhimw6ybHwhSFmP4C2AZRSZ";
+
+techTestApp.init = function() {
+};
+
+techTestApp.url = new URL("https://quizapi.io/api/v1/questions");
+techTestApp.url.search = new URLSearchParams({
+    apiKey: techTestApp.apiKey,
+    category: "Code",
+    tags: "javascript",
+    difficulty: "easy"
+})
+
+fetch(techTestApp.url)
+.then(function(response) {
+  // console.log(response);
+  return response.json();
+})
+.then(function(jsonResponse) {
+  // console.log(jsonResponse);
+
+  const question1 = jsonResponse[0].question;
+
+  const answers1 = jsonResponse[0].answers;
+  const answerArray1 = Object.values(answers1);
+
+  // const correctAnswer1 = jsonResponse[0].correct_answer[0];
+
+  const correctAnswers1 = jsonResponse[0].correct_answers;
+  const correctAnswersArray1 = Object.values(correctAnswers1);
+
+  //appending the question
+  const h3 = document.querySelector("h3");
+  h3.innerText = question1;
+
+  //appending the answers below, so long as the answer doesn't contain a null value
+  const ul = document.querySelector("ul");
+  for(i = 0; i < answerArray1.length; i++) {
+  if(answerArray1[i] !== null) {
+    const li = document.createElement("li");
+    li.innerText = answerArray1[i];
+    ul.appendChild(li);
+  }
+}
+//once answers are listed, run event listener for options clicked
+techTestApp.answerSelect();
+
+console.log(question1);
+console.log(answerArray1);
+// console.log(correctAnswer1);
+console.log(correctAnswersArray1);
+})
+
+techTestApp.answerSelect = function() {
+  const options = document.querySelectorAll("li");
+  
+  options.forEach(function(clickOption) {
+    clickOption.addEventListener("click", function() {
+      document.querySelectorAll("li").classList.toggle("selected");
+      console.log("Something was clicked!");
+    })
+  })
+};
+
+
+techTestApp.init();
